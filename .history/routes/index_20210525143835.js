24228f121c9066986930842ead833f933067b356
Mateo Davila <matedavilai@gmail.com>
2021-05-28 16:44:44 -0400

    initial commit

---
+++ .history/routes/index_20210525143835.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Home', });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', 
+  { title: 'Express', });
+});
+
+/* GET products page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Express', });
+});
+
+/* GET services page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Express', });
+});
+
+/* GET about us page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Express', });
+});
+
+/* GET contact us page. */
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { title: 'Express', });
+});
+
+module.exports = router;
